b8a425deb3e79a1970621695dd472ca1454459d3
JoSantana0005 <joseasantana05@gmail.com>
2025-03-28 19:52:25 -0400

    Primera navegación , ahora hacer los componentes de cada aplicación

---
+++ Simulate/app/index.js
@@ -0,0 +1,34 @@
+import { BarraIcon } from "../Components/BarraIcon";
+import { InfoClimate } from "../Components/InfoClimate";
+import { HoursDate } from "../Components/HoursDate";
+import { AplicationsRight } from "../Components/AplicationsRight";
+import { AplicationsBottom } from "../Components/AplicationsBottom";
+import { StylesGeneral } from "../Css/General";
+import { View, ImageBackground } from "react-native";
+import { useBattery } from "../Js/Battery";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+
+export default function App(){
+    const [batteryLevel, setBatteryLevel, backgroundColor, setBackgroundColor] = useBattery();
+    return (
+        <SafeAreaProvider>
+            {batteryLevel > 0 ? (
+                <View style={StylesGeneral.Container}>
+                    <ImageBackground source={require('../assets/Fondo.jpg')} style={StylesGeneral.ImagenBackground}>
+                        <BarraIcon/>
+                        <HoursDate/>
+                    <View style={StylesGeneral.ContainerAplications}>
+                        <InfoClimate/>
+                        <AplicationsRight/>
+                    </View>
+                    <View style={StylesGeneral.ContainerAplications_2}>
+                        <AplicationsBottom/>
+                    </View>
+                    </ImageBackground>
+                </View>
+            ) : (
+                <View style={[StylesGeneral.Container, {backgroundColor: backgroundColor}]}></View>
+            )}
+        </SafeAreaProvider>
+    );
+}
